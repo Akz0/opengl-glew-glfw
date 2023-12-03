1729d2580ba38b17d1749eb9886c7e55d1f324cb
Om Lachake <omlachake@pm.me>
2023-12-03 17:19:06 +0000

    Types of Lights - Point, Direction, Spot

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -14,8 +14,17 @@ uniform vec4 lightColor;
 uniform vec3 lightPosition;
 uniform vec3 CameraPosition;
 
-void main()
-{
+
+vec4 PointLight(){
+	
+	vec3 LightVector = lightPosition - CurrentPosition;
+	float distance = length(LightVector);
+
+	float a = 3.1f;
+	float b = 1.2f;
+
+	float intensity = 1.0f / (a*distance*distance + b*distance + 1.0f);
+	
 	vec3 normal = normalize(Normal);
 	vec3 lightDirection = normalize(lightPosition - CurrentPosition);
 	float diffuse = max(dot(normal, lightDirection), 0.0f);
@@ -28,5 +37,48 @@ void main()
 
 	float ambient = 0.20f;
 
-	FragColor = texture(tex0, TexCord) * lightColor * (diffuse + ambient) + texture(tex1, TexCord).r * specular;
+	return (texture(tex0, TexCord) * (diffuse + ambient + intensity) + texture(tex1, TexCord).r * specular) * lightColor;
+}
+
+vec4 SpotLight(){
+	float outerCone = 0.90f;
+	float innerCone = 0.95f;
+	
+	vec3 normal = normalize(Normal);
+	vec3 lightDirection = normalize(lightPosition - CurrentPosition);
+	float diffuse = max(dot(normal, lightDirection), 0.0f);
+
+	float SpecularLight = 0.5f;
+	vec3 ViewDirection = normalize(CameraPosition - CurrentPosition);
+	vec3 ReflectionDirection = reflect(-lightDirection, normal);
+	float SpecularAmount = pow(max(dot(ViewDirection,ReflectionDirection),0.0f),16);
+	float specular = SpecularAmount * SpecularLight;
+
+	float ambient = 0.20f;
+
+	float angle = dot(vec3(0.0f,-1.0f,0.0f),-lightDirection);
+	float intensity = clamp((angle - outerCone) / (innerCone - outerCone),0.0f,1.0f);
+	return (texture(tex0, TexCord) * (diffuse + ambient + intensity) + texture(tex1, TexCord).r * specular *intensity) * lightColor;
+}
+
+vec4 DirectionLight(){
+	
+	vec3 normal = normalize(Normal);
+	vec3 lightDirection = normalize(vec3(1.0f,1.0f,0.0f));
+	float diffuse = max(dot(normal, lightDirection), 0.0f);
+
+	float SpecularLight = 0.5f;
+	vec3 ViewDirection = normalize(CameraPosition - CurrentPosition);
+	vec3 ReflectionDirection = reflect(-lightDirection, normal);
+	float SpecularAmount = pow(max(dot(ViewDirection,ReflectionDirection),0.0f),16);
+	float specular = SpecularAmount * SpecularLight;
+
+	float ambient = 0.20f;
+
+	return (texture(tex0, TexCord) * (diffuse + ambient ) + texture(tex1, TexCord).r * specular) * lightColor;
+}
+
+void main()
+{
+	FragColor = DirectionLight();
 }
